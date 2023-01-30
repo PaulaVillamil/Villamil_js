e7466a9efae7899ca30331dd1c70c083cb8814f5
Paula Villamil <hi.pauvilla@gmail.com>
2023-01-30 23:19:57 +0100

    primer_preentrega

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let nombre;
+let correo;
+let comprueba=Nan;
+let cantidadPiezas;
+let precioFinal;
+let datosPago;
+
+nombre= (prompt("Ingrese su nombre"))
+
+if (nombre==comprueba) 
+    
+{correo= (prompt ("Ingrese su correo"))}
+else 
+{nombre= (prompt("Ingrese su nombre"))};
+
+//correo= (prompt ("Ingrese su correo"))
+
+//cantidadPiezas = parseInt (prompt (nombre + ", ¿cuántas piezas desea reservar?"))
+
+//let esNum = Number; 
+//if (esNum) 
+//{true} else {false};
+
+//if (cantidadPiezas) {
+   // Nan;
+    //} alert (nombre+", por favor, ingresa un número");
+
+
+//alert (nombre + ", estas a punto de reservar " + cantidadPiezas + " piezas para el próximo taller")
